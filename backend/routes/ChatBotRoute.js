39a126fe10c98a856ccf12dc6787e698578e53ce
iqbalrsyd <iqbalhidayatrasyad@mail.ugm.ac.id>
2025-05-30 03:20:27 +0700

    menambahkan mode chatbot untuk create event dengan prompt

--- backend/routes/ChatBotRoute.js
+++ backend/routes/ChatBotRoute.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import { checkConflict } from '../controllers/ChatBotController.js';
+import verifyToken from "../middleware/verifyToken.js";
+
 
 const router = express.Router();
-router.post('/check', checkConflict);
+router.post("/check", verifyToken, checkConflict);
 
 export default router;
